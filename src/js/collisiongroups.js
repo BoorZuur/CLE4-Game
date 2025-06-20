01ecb3d5a7592be051c15bd9e1ecca6b79f4fc17
Henk-Jan Polak <125260882+HenkHR@users.noreply.github.com>
2025-06-20 11:40:40 +0200

    Delete src/js directory

--- src/js/collisiongroups.js
+++
@@ -1,5 +0,0 @@
-import { CollisionGroupManager } from "excalibur"
-
-export const friendsGroup = CollisionGroupManager.create('friends')
-export const platformP = CollisionGroupManager.create('platformP')
-export const pressure = CollisionGroupManager.create('pressure')
